ac1a89b89c602d1921586cdfeed8fab28cfec3e0
Glen Husman <wappokickingdeer@gmail.com>
2016-11-13 17:24:08 -0800

    Overload equality and hashcode on Size coretype Uses method recommended by Jon Skeet on Stack Exchange

--- ConsoleGameLib/CoreTypes/Size.cs
+++ ConsoleGameLib/CoreTypes/Size.cs
@@ -15,5 +15,39 @@ namespace ConsoleGameLib.CoreTypes
 
         public int Width;
         public int Height;
+
+        public override int GetHashCode()
+        {
+            // Uses http://stackoverflow.com/questions/263400/what-is-the-best-algorithm-for-an-overridden-system-object-gethashcode
+            unchecked // Overflow is fine, just wrap
+            {
+                int hash = 29;
+                hash = hash * 89 + Width.GetHashCode();
+                hash = hash * 89 + Height.GetHashCode();
+                return hash;
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is Size))
+            {
+                return false;
+            }
+            Size oth = (Size)obj;
+            return Width == oth.Width && Height == oth.Height;
+        }
+
+        public static bool operator ==(Size left, Size right)
+        {
+            // Struct, so no need for null check
+            return left.Width == right.Width && left.Height == right.Height;
+        }
+
+        public static bool operator !=(Size left, Size right)
+        {
+            // Struct, so no need for null check
+            return left.Width != right.Width || left.Height != right.Height;
+        }
     }
 }
