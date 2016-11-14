4c458f5a53439f8c57208c228ee580e93e41138d
Glen Husman <wappokickingdeer@gmail.com>
2016-11-13 17:30:13 -0800

    Overrode Point equality and hashcode

--- ConsoleGameLib/CoreTypes/Point.cs
+++ ConsoleGameLib/CoreTypes/Point.cs
@@ -15,5 +15,39 @@ namespace ConsoleGameLib.CoreTypes
 
         public int X;
         public int Y;
+
+        public override int GetHashCode()
+        {
+            // Uses http://stackoverflow.com/questions/263400/what-is-the-best-algorithm-for-an-overridden-system-object-gethashcode
+            unchecked // Overflow is fine, just wrap
+            {
+                int hash = 37;
+                hash = hash * 67 + X.GetHashCode();
+                hash = hash * 67 + Y.GetHashCode();
+                return hash;
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is Point))
+            {
+                return false;
+            }
+            Point oth = (Point)obj;
+            return X == oth.X && Y == oth.Y;
+        }
+
+        public static bool operator ==(Point left, Point right)
+        {
+            // Struct, so no need for null check
+            return left.X == right.X && left.Y == right.Y;
+        }
+
+        public static bool operator !=(Point left, Point right)
+        {
+            // Struct, so no need for null check
+            return left.X != right.X || left.Y != right.Y;
+        }
     }
 }
